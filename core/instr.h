04479d0d7bca76527252ab62280ef42a12aeda69
HardGraphite <jodieydchen@gmail.com>
2024-01-25 14:30:36 +0800

    Fix instruction compose and extra errors

--- core/instr.h
+++ core/instr.h
@@ -27,6 +27,22 @@ enum zis_op_type {
     ZIS_OP_ABsCs,
 };
 
+#define ZIS_INSTR_U25_MAX  ((1 << 25) - 1)
+#define ZIS_INSTR_I25_MAX  ((1 << 24) - 1)
+#define ZIS_INSTR_I25_MIN  (-(1 << 24))
+
+#define ZIS_INSTR_U16_MAX  ((1 << 16) - 1)
+#define ZIS_INSTR_I16_MAX  ((1 << 15) - 1)
+#define ZIS_INSTR_I16_MIN  (-(1 << 15))
+
+#define ZIS_INSTR_U9_MAX  ((1 << 9) - 1)
+#define ZIS_INSTR_I9_MAX  ((1 << 8) - 1)
+#define ZIS_INSTR_I9_MIN  (-(1 << 8))
+
+#define ZIS_INSTR_U8_MAX  ((1 << 8) - 1)
+#define ZIS_INSTR_I8_MAX  ((1 << 7) - 1)
+#define ZIS_INSTR_I8_MIN  (-(1 << 7))
+
 #define zis_instr_make_Aw(OP, Aw) \
     ((uint32_t)(OP) | (uint32_t)(Aw) << 7)
 
@@ -34,16 +50,16 @@ enum zis_op_type {
     ((uint32_t)(OP) | (int32_t)(Asw) << 7)
 
 #define zis_instr_make_ABw(OP, A, Bw) \
-    ((uint32_t)(OP) | ((uint32_t)(A) & 0x11f) << 7 | (uint32_t)(Bw) << 16)
+    ((uint32_t)(OP) | ((uint32_t)(A) & 0x1ff) << 7 | (uint32_t)(Bw) << 16)
 
 #define zis_instr_make_ABsw(OP, A, Bsw) \
-    ((uint32_t)(OP) | ((uint32_t)(A) & 0x11f) << 7 | (int32_t)(Bsw) << 16)
+    ((uint32_t)(OP) | ((uint32_t)(A) & 0x1ff) << 7 | (int32_t)(Bsw) << 16)
 
 #define zis_instr_make_ABC(OP, A, B, C) \
     ((uint32_t)(OP) | ((uint32_t)(A) & 0x1ff) << 7 | ((uint32_t)(B) & 0xff) << 16 | (uint32_t)(C) << 24)
 
-#define zis_instr_make_ABsCs(OP, A, B, C) \
-    ((uint32_t)(OP) | ((uint32_t)(A) & 0x1ff) << 7 | ((int32_t)(B) & 0xff) << 16 | (int32_t)(C) << 24)
+#define zis_instr_make_ABsCs(OP, A, Bs, Cs) \
+    ((uint32_t)(OP) | ((uint32_t)(A) & 0x1ff) << 7 | ((int32_t)(Bs) & 0xff) << 16 | (int32_t)(Cs) << 24)
 
 #define zis_instr_extract_opcode(I) \
     ((I) & 0x7f)
@@ -77,9 +93,9 @@ do {                                               \
     C = (uint32_t)(I) >> 24;                       \
 } while (0)
 
-#define zis_instr_extract_operands_ABsCs(I, A, B, C) \
-do {                                                 \
-    A = (uint32_t)(I) >> 7 & 0x1ff;                  \
-    B = (int32_t)(I) >> 16 & 0xff;                   \
-    C = (int32_t)(I) >> 24;                          \
+#define zis_instr_extract_operands_ABsCs(I, A, Bs, Cs) \
+do {                                                   \
+    A = (uint32_t)(I) >> 7 & 0x1ff;                    \
+    Bs = ((int32_t)(I) << 8) >> 24;                    \
+    Cs = (int32_t)(I) >> 24;                           \
 } while (0)
