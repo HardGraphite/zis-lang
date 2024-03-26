bcd50608f70665699b7ff433c3bc65c3bc7564e7
HardGraphite <jodieydchen@gmail.com>
2024-03-26 13:37:22 +0800

    Fix stream object being deleted during compilation of big files

--- core/compile.c
+++ core/compile.c
@@ -11,15 +11,19 @@
 #if ZIS_FEATURE_SRC
 
 struct zis_func_obj *zis_compile_source(
-    struct zis_context *z, struct zis_stream_obj *input,
+    struct zis_context *z, struct zis_stream_obj *_input,
     struct zis_module_obj *_module
 ) {
     struct zis_func_obj *func = NULL;
-    zis_locals_decl_1(z, var, struct zis_module_obj *module);
-    var.module = _module;
+    zis_locals_decl(
+        z, var,
+        struct zis_stream_obj *input;
+        struct zis_module_obj *module;
+    );
+    var.input = _input, var.module = _module;
 
     struct zis_parser *parser = zis_parser_create(z);
-    struct zis_ast_node_obj *ast = zis_parser_parse(parser, input, ZIS_PARSER_MOD);
+    struct zis_ast_node_obj *ast = zis_parser_parse(parser, var.input, ZIS_PARSER_MOD);
     zis_parser_destroy(parser, z);
     if (ast) {
         struct zis_codegen *codegen = zis_codegen_create(z);
