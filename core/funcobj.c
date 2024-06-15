6223984957c2236c6acb558a87e8b57c3e52c0d0
HardGraphite <jodieydchen@gmail.com>
2024-06-15 21:59:42 +0800

    Implement methods for type `Function`

--- core/funcobj.c
+++ core/funcobj.c
@@ -3,10 +3,15 @@
 #include <assert.h>
 #include <string.h>
 
+#include "algorithm.h"
 #include "context.h"
 #include "globals.h"
+#include "invoke.h"
 #include "ndefutil.h"
 #include "objmem.h"
+#include "stack.h"
+
+#include "tupleobj.h"
 
 static_assert(sizeof(struct zis_func_obj_meta) <= sizeof(void *), "");
 static_assert(sizeof(struct zis_native_func_meta) == sizeof(struct zis_func_obj_meta), "");
@@ -103,8 +108,49 @@ size_t zis_func_obj_bytecode_length(const struct zis_func_obj *self) {
     return (self->_bytes_size - FUN_OBJ_BYTES_FIXED_SIZE) / sizeof(zis_func_obj_bytecode_word_t);
 }
 
+#define assert_arg1_Function(__z) \
+    (assert(zis_object_type_is((__z)->callstack->frame[1], (__z)->globals->type_Function)))
+
+ZIS_NATIVE_FUNC_DEF(T_Function_M_operator_call, z, {1, (unsigned char)-1, 2}) {
+    /*#DOCSTR# func Function:\'()'(*args) :: Any
+    Call the function. */
+    assert_arg1_Function(z);
+    struct zis_object **frame = z->callstack->frame;
+    assert(zis_object_type_is(frame[2], z->globals->type_Tuple));
+    const size_t argc = zis_tuple_obj_length(zis_object_cast(frame[2], struct zis_tuple_obj));
+    struct zis_func_obj *func = zis_invoke_prepare_pa(z, frame[1], frame, frame[2], argc);
+    if (zis_unlikely(!func))
+        return ZIS_THR;
+    assert(zis_object_from(func) == frame[1]);
+    return zis_invoke_func(z, func);
+}
+
+ZIS_NATIVE_FUNC_DEF(T_Function_M_hash, z, {1, 0, 1}) {
+    /*#DOCSTR# func Function:hash() :: Int
+    Generates hash code. */
+    assert_arg1_Function(z);
+    struct zis_object **frame = z->callstack->frame;
+    struct zis_func_obj *const self = zis_object_cast(frame[1], struct zis_func_obj);
+    size_t hash_code;
+    if (self->native) {
+        void *p = (void *)(uintptr_t)self->native;
+        hash_code = zis_hash_pointer(p);
+    } else {
+        void *p = self; // Bytecode function object won't be moved by GC.
+        hash_code = zis_hash_pointer(p);
+    }
+    frame[0] = zis_smallint_to_ptr((zis_smallint_t)zis_hash_truncate(hash_code));
+    return ZIS_OK;
+}
+
+ZIS_NATIVE_FUNC_DEF_LIST(
+    T_Function_D_methods,
+    { "()"      , &T_Function_M_operator_call   },
+    { "hash"    , &T_Function_M_hash            },
+);
+
 ZIS_NATIVE_TYPE_DEF_XB(
     Function,
     struct zis_func_obj, _bytes_size,
-    NULL, NULL, NULL
+    NULL, T_Function_D_methods, NULL
 );
